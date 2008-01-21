fb160464fb138e0d2c6cff04168eed089fb00bef
Carlo Baldassi <carlobaldassi@yahoo.it>
2008-01-21 01:55:38 +0100

    Relicensed under GPLv2 or later

--- src/render.c
+++ src/render.c
@@ -1,6 +1,5 @@
 /* GIMP LiquidRescaling Plug-in
  * Copyright (C) 2007 Carlo Baldassi (the "Author") <carlobaldassi@yahoo.it>.
- * (implementation based on the GIMP Plug-in Template by Michael Natterer)
  * All Rights Reserved.
  *
  * This plugin implements the algorithm described in the paper
@@ -10,7 +9,8 @@
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; version 2 dated June, 1991.
+ * the Free Software Foundation; either version 2 of the Licence, or
+ * (at your option) any later version.
 
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -18,9 +18,7 @@
  * GNU General Public License for more details.
  
  * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
- * 02111-1307, USA.
+ * along with this program; if not, see <http://www.gnu.org.licences/>.
  */
 
 #include "config.h"
