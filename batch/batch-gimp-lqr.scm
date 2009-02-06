aa6d86d5e893063b1a89897808b462fa71991c8d
Carlo Baldassi <carlobaldassi@gmail.com>
2009-02-06 17:23:50 +0100

    Updated help file images (en+it), minor fixes (0.6 release candidate)

--- batch/batch-gimp-lqr.scm
+++ batch/batch-gimp-lqr.scm
@@ -1,3 +1,21 @@
+; GIMP LiquidRescale Plug-in
+; Copyright (C) 2007-2009 Carlo Baldassi (the "Author") <carlobaldassi@gmail.com>.
+; All Rights Reserved.
+;
+; This program is free software; you can redistribute it and/or modify
+; it under the terms of the GNU General Public License as published by
+; the Free Software Foundation; either version 2 of the Licence, or
+; (at your option) any later version.
+;
+; This program is distributed in the hope that it will be useful,
+; but WITHOUT ANY WARRANTY; without even the implied warranty of
+; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+; GNU General Public License for more details.
+;
+; You should have received a copy of the GNU General Public License
+; along with this program; if not, see <http://www.gnu.org.licences/>.
+;
+
 (define
   (batch-gimp-lqr			; run with default arguments, except for:
 	filename			; (STRING) filname
